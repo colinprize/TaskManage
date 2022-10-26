11a146982ce85d613ba854c2732ea6299a1ab8b3
Colin Prise <devites@gmail.com>
2022-10-25 19:23:38 -0700

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -23,5 +23,6 @@ def redirect_to_home(request):
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
-    path("", redirect_to_home, name="home")
+    path("", redirect_to_home, name="home"),
+    path("accounts/", include("accounts.urls"))
 ]
